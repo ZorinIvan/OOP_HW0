f5426990b800cc4c2ca15ffbda290e65c214aba5
ZorinIvan <ivanzorin.mail@gmail.com>
2016-11-12 14:17:53 +0200

    parser change 1

--- src/homework0/Parser.java
+++ src/homework0/Parser.java
@@ -7,34 +7,21 @@ import java.lang.String;
 public class Parser {
 
 
-	public static void main(String[] args) throws FileNotFoundException, IOException{ 
+	public static void main(String[] args) throws IOException{ 
 		Scanner scanner = new Scanner(System.in);
 		String fileName = scanner.next();
 		scanner.close();
 		
-		//FileReader fr = new FileReader(fileName);
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-		String line = bufferedReader.readLine();
-		while(line != null)
+		FileReader fr = new FileReader(fileName);
+		BufferedReader bufferedReader = new BufferedReader(fr);
+		String line;
+		
+		while((line= bufferedReader.readLine()) != null)
 		{
-			String delims = "//";
-			StringTokenizer st1 = new StringTokenizer(line, delims);
-			if(st1.hasMoreElements())
-			{
-				System.out.println(st1.nextElement());
-			}
-			else 
-			{
-				System.out.println(st1);
-			}
-			
-			StringTokenizer st2 = new StringTokenizer(line, "/*");
-			if(st2.hasMoreElements())
-			{
-				System.out.println(st1.nextToken("*/"));
-			}
-			
-		}
+			String[] result = line.split("//");
+			if(result.)
+			System.out.println(result[1]);
+		}//while
 		
 		bufferedReader.close();
 	}
