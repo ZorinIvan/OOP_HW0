6a58ecd4d625e8e7f3e83205ae56ddb22d95b6ff
ZorinIvan <ivanzorin.mail@gmail.com>
2016-11-12 18:32:28 +0200

    ex2. handles errors

--- src/homework0/Parser.java
+++ src/homework0/Parser.java
@@ -8,31 +8,45 @@ public class Parser {
 
 
 	public static void main(String[] args) throws IOException{ 
-		Scanner scanner = new Scanner(System.in);
-		String fileName = scanner.next();
-		scanner.close();
-		
-		FileReader fr = new FileReader(fileName);
-		BufferedReader bufferedReader = new BufferedReader(fr);
-		String line;
-		
-		while((line= bufferedReader.readLine()) != null)
+		Scanner console = new Scanner(System.in);
+		Scanner lineTokenizer = new Scanner(console.nextLine());
+		String fileName = lineTokenizer.next();
+		if(lineTokenizer.hasNext())
 		{
-			String[] result1 = line.split("//");
-			String[] result2 = line.split("/+\\*");
-			if(result1.length != 1)
-			{
-				System.out.println(result1[1]);
-			}
-			if (result2.length != 1)
-			{
-				String[] result3 = result2[1].split("\\*+/");
-				System.out.println(result3[0]);
-			}
-
-		}//while
+			System.err.println("Error. Wrong number of arguments" );
+			console.close();
+			lineTokenizer.close();
+			return;
+		}
+		console.close();
+		lineTokenizer.close();
 		
-		bufferedReader.close();
+		try{
+			FileReader fr = new FileReader(fileName);
+	
+			BufferedReader bufferedReader = new BufferedReader(fr);
+			String line;
+			
+			while((line= bufferedReader.readLine()) != null)
+			{
+				String[] result1 = line.split("//");
+				String[] result2 = line.split("/+\\*");
+				if(result1.length != 1)
+				{
+					System.out.println(result1[1]);
+				}
+				if (result2.length != 1)
+				{
+					String[] result3 = result2[1].split("\\*+/");
+					System.out.println(result3[0]);
+				}
+	
+			}//while
+			
+			bufferedReader.close();
+		} catch (FileNotFoundException fileNotFound){
+			 System.err.println("Error. File \"" + fileName + "\" not found" );
+		}
 	}
 
 }
