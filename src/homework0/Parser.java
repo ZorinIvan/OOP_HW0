4ff50000ced2db351e582867a9aa9df88fcb0a8b
ZorinIvan <ivanzorin.mail@gmail.com>
2016-11-12 14:55:01 +0200

    ex2. find line comments // + /*...*/

--- src/homework0/Parser.java
+++ src/homework0/Parser.java
@@ -18,9 +18,18 @@ public class Parser {
 		
 		while((line= bufferedReader.readLine()) != null)
 		{
-			String[] result = line.split("//");
-			if(result.)
-			System.out.println(result[1]);
+			String[] result1 = line.split("//");
+			String[] result2 = line.split("/+\\*");
+			if(result1.length != 1)
+			{
+				System.out.println(result1[1]);
+			}
+			if (result2.length != 1)
+			{
+				String[] result3 = result2[1].split("\\*+/");
+				System.out.println(result3[0]);
+			}
+
 		}//while
 		
 		bufferedReader.close();
