deba51399b877ced8ab62fce8ad40fb7e9fe8ae6
ZorinIvan <ivanzorin.mail@gmail.com>
2016-11-14 21:02:49 +0200

    NOT comand line arguments

--- src/homework0/Parser.java
+++ src/homework0/Parser.java
@@ -6,25 +6,17 @@ import java.lang.String;
 
 public class Parser {
 
- /*test*/
+
 	public static void main(String[] args) throws IOException{ 
-		Scanner console = new Scanner(System.in);
-		Scanner lineTokenizer = new Scanner(console.nextLine());
-		String fileName = lineTokenizer.next();
-		if(lineTokenizer.hasNext())
+
+		String fileName = getInput();
+		if(fileName == null)
 		{
-			System.err.println("Error. Wrong number of arguments" );
-			console.close();
-			lineTokenizer.close();
 			return;
 		}
-		console.close();
-		lineTokenizer.close();
-		
 		try{
-			FileReader fr = new FileReader(fileName);
 	
-			BufferedReader bufferedReader = new BufferedReader(fr);
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
 			String line;
 			
 			while((line= bufferedReader.readLine()) != null)
@@ -35,10 +27,18 @@ public class Parser {
 				{
 					System.out.println(result1[1]);
 				}
-				if (result2.length != 1)
+				if (result2.length != 1) // found /*
 				{
-					String[] result3 = result2[1].split("\\*+/");
-					System.out.println(result3[0]);
+					while((line= bufferedReader.readLine()) != null)
+					{
+						String[] result3 = result2[1].split("\\*+/");
+						if (result3.length != 1)
+						{
+							System.out.println(result3[0]);
+						}
+						System.out.println(line);
+					}
+
 				}
 	
 			}//while
@@ -47,6 +47,28 @@ public class Parser {
 		} catch (FileNotFoundException fileNotFound){
 			 System.err.println("Error. File \"" + fileName + "\" not found" );
 		}
+	}
+	
+
+	/**
+	 * @return file name if input is valid
+	 * otherwise returns null
+	 */
+	public static String getInput (){
+		Scanner console = new Scanner(System.in);
+		Scanner lineTokenizer = new Scanner(console.nextLine());
+		String fileName = lineTokenizer.next();
+		
+		if(lineTokenizer.hasNext())
+		{
+			System.err.println("Error. Wrong number of arguments" );
+			console.close();
+			lineTokenizer.close();
+			return null;
+		}
+		console.close();
+		lineTokenizer.close();
+		return fileName;
 	}
 
 }
